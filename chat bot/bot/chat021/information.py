0d8615e7143c99c3dcee1d8fec6a8ef97e10f479
santoshsharma12345 <sanstanley123@gmail.com>
2023-08-14 21:02:52 +0545

    first push

---
+++ chat bot/bot/chat021/information.py
@@ -0,0 +1,13 @@
+# message Greeting
+greeting = ["Hello sir, Please tell me how may I help you?","Nice to meet you?, Is there anything I can help you with?","Hi I'm google, How can I help you?","How can I help you?, Sir"]
+
+
+close =  ["Sad to see you go :(", "Talk to you later", "Goodbye! See you again", "Goodbye, Have a Nice Day."]
+ 
+news = ["barcelona draws with getafe", "exam is on 17th of bhadra","Happy international youth day"]
+
+temp = [ '19°C', '36°C',' 31°C', '24°C', '26°C ']
+
+breakfast=["dal roti", "Cereal and Milk", "Yogurt Parfait", "Breakfast Sandwich"]
+
+plans=["meeting with political", "presentation", "Go for movies"]
